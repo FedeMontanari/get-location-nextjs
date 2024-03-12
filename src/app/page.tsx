a2a621c662f0e544ee52f942e1b2b569060a4b8b
Yaki <fedemontanari@outlook.com>
2024-03-12 09:43:27 -0300

    Added dynamic loading for Map component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import Map from "@/components/Map";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 export default function Home() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/Map"), {
+        loading: () => <p>A map is loading...</p>,
+        ssr: false,
+      }),
+    []
+  );
+
   return (
     <main className="w-screen h-screen flex items-center justify-center flex-col gap-4">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -16,6 +26,13 @@ export default function Home() {
         <i>
           Location data does <b>NOT</b> get stored anywhere and its deleted when
           you close/refresh the tab or close the browser.
+          <br />
+          <a
+            href="https://github.com/FedeMontanari/get-location-nextjs"
+            className="text-blue-400 hover:border-b-2 hover:font-bold border-blue-400"
+          >
+            Source Code
+          </a>
         </i>
       </p>
     </main>
